5b747f6819bbc04d155e5c96909c51e18cc0b76e
Stiven Ramírez Arango <stivenramirez@habi.co>
2022-06-28 22:37:10 -0500

    [ADD] Jobs and Ops

--- tests/complex_asset_graph/test_complex_asset_graph.py
+++ tests/complex_asset_graph/test_complex_asset_graph.py
@@ -2,12 +2,17 @@ import pytest
 
 from dagster import materialize
 
-from complex_asset_graph import (
+from src.complex_asset_graph import (
     nabisco_cereals,
     cereals,
     cereal_protein_fractions,
     highest_protein_nabisco_cereal,
 )
+
+
+@pytest.fixture(scope='module')
+def cereals():
+    return [{'name': 'cereal1', 'mfr': 'N'}, {'name': 'cereal2', 'mfr': 'K'}]
 
 
 @pytest.fixture(scope='module')
@@ -18,6 +23,12 @@ def assets():
         cereal_protein_fractions,
         highest_protein_nabisco_cereal,
     ]
+
+
+def test_nabisco_cereals(cereals):
+    result = nabisco_cereals(cereals)
+    assert len(result) == 1
+    assert result == [{'name': 'cereal1', 'mfr': 'N'}]
 
 
 def test_cereal_assets(assets):
